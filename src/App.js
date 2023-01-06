bf9bac5ae95a76dbfe8eee5a3d558fef31044b84
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2023-01-06 17:05:24 +0100

    Add a final version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,164 @@
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const letters = 'abcdefghijklmnoprstuvwxyz';
+  const numbers = '0123456789';
+  const specialSigns = '!@#$%&';
+
+  const [password, setPassword] = useState('');
+
+  const [form, setForm] = useState({
+    lowercase: false,
+    uppercase: false,
+    numbers: false,
+    specialsigns: false,
+    length: 8,
+  });
+
+  const handleSignsChange = (e) => {
+    const name = e.target.name;
+    const checked = e.target.checked;
+
+    setForm((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: checked,
+      };
+    });
+  };
+  const handleLengthChange = (e) => {
+    const value = e.target.value;
+
+    if (value < 1) {
+      setForm((prevValue) => {
+        return {
+          ...prevValue,
+          length: 1,
+        };
+      });
+      return;
+    }
+
+    setForm((prevValue) => {
+      return {
+        ...prevValue,
+        length: value,
+      };
+    });
+  };
+
+  const generatePassword = (e) => {
+    e.preventDefault();
+
+    let text = '';
+
+    if (form.lowercase === true) {
+      text += letters;
+    }
+    if (form.uppercase === true) {
+      text += letters.toUpperCase();
+    }
+    if (form.numbers === true) {
+      text += numbers;
+    }
+    if (form.specialsigns === true) {
+      text += specialSigns;
+    }
+
+    if (text.length === 0) {
+      alert('Check the form!');
+      return;
+    }
+
+    let password = '';
+
+    for (let i = 0; i < form.length; i++) {
+      const index = Math.floor(Math.random() * text.length);
+      password += text[index];
+    }
+
+    setPassword(password);
+  };
+
+  return (
+    <div className="App">
+      <div className="box">
+        <h1 className="title">Password generator</h1>
+        <form className="form" onSubmit={generatePassword}>
+          <label htmlFor="lowercase">
+            lowercase:
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="lowercase"
+              id="lowercase"
+              onChange={handleSignsChange}
+            />
+          </label>
+          <label htmlFor="uppercase">
+            uppercase:
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="uppercase"
+              id="uppercase"
+              onChange={handleSignsChange}
+            />
+          </label>
+          <label htmlFor="numbers">
+            numbers:
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="numbers"
+              id="numbers"
+              onChange={handleSignsChange}
+            />
+          </label>
+          <label htmlFor="specialsigns">
+            specialsigns:
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="specialsigns"
+              id="specialsigns"
+              onChange={handleSignsChange}
+            />
+          </label>
+          <div className="lengthChange">
+            <label htmlFor="length">
+              length:
+              <input
+                type="number"
+                name="length"
+                id="length"
+                min={1}
+                onChange={handleLengthChange}
+                value={form.length}
+              />
+            </label>
+            <label htmlFor="range">
+              <input
+                className="range"
+                type="range"
+                name="range"
+                id="range"
+                min={1}
+                onChange={handleLengthChange}
+                value={form.length}
+              />
+            </label>
+          </div>
+          <input className="submitButton" type="submit" value="Generate" />
+        </form>
+        <div className="password">
+          {password ? password : <p>Generate your password!</p>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
